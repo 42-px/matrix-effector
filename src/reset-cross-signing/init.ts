b319d956a8b8d3b2914c76687f55872285288185
skyka13711 <nikitos13711@gmail.com>
2022-11-03 15:03:11 +0300

    WIP

--- src/reset-cross-signing/init.ts
+++ src/reset-cross-signing/init.ts
@@ -1,53 +1,97 @@
 import { IAuthData, InteractiveAuth } from "matrix-js-sdk"
+import { sample } from "effector"
 
 import { client } from "@/matrix-client"
-import { confirmResetCrossSigningFx } from "./public"
+import { LOGIN_BY_PASSWORD } from "@/constants"
 
+import { 
+    confirmResetCrossSigningFx, 
+    onInteractiveAuthBusyChange, 
+    onInteractiveAuthStateUpdate, 
+    onNeedUserPassword,
+    onUserPasswordError,
+    submitAuthDict
+} from "./public"
+import { 
+    $interactiveAuthInstance,
+    setInteractiveAuth,
+    submitAuthDictFx
+} from "./private"
+import { ConfirmResetCrossSigningFxResult } from "./types"
+
+$interactiveAuthInstance
+    .on(setInteractiveAuth, (_, auth) => auth)
+
+sample({
+    clock: submitAuthDict,
+    source: $interactiveAuthInstance,
+    filter: (
+        isInstance: InteractiveAuth | null,
+    ): isInstance is InteractiveAuth => Boolean(isInstance),
+    fn: (auth, pass) => ({
+        password: pass,
+        interactiveAuth: auth
+    }),
+    target: submitAuthDictFx
+})
+
+sample({
+    source: onInteractiveAuthStateUpdate,
+    filter: ({status}) => status.errcode === "M_FORBIDDEN",
+    fn: ({status}) => status.error as string,
+    target: onUserPasswordError,
+})
 
 confirmResetCrossSigningFx.use(async () => {
     const cl = client()
-    await cl.bootstrapCrossSigning({
-        authUploadDeviceSigningKeys: async (makeRequest) => {
-            const requestCallback = (
-                auth: IAuthData, 
-                background: boolean
-            ): Promise<IAuthData> => {
-                return makeRequest(auth)
-            }
-            const authLogic = new InteractiveAuth({
-                authData: undefined,
-                doRequest: requestCallback,
-                busyChanged: console.log,
-                stateUpdated: console.log.bind(undefined, "stateUpdated"),
-                matrixClient: cl,
-                sessionId: undefined,
-                clientSecret: undefined,
-                emailSid: undefined,
-                requestEmailToken: console.log as any,
-            })
-            authLogic.attemptAuth().then((result) => {
-                const extra = {
-                    emailSid: authLogic.getEmailSid(),
-                    clientSecret: authLogic.getClientSecret(),
+    const promise = new Promise<
+        ConfirmResetCrossSigningFxResult
+    >((res, rej) => {
+        cl.bootstrapCrossSigning({
+            
+            authUploadDeviceSigningKeys: async (makeRequest) => {
+                const requestCallback = (
+                    auth: IAuthData, 
+                ): Promise<IAuthData> => {
+                    return makeRequest(auth)
                 }
-                console.log("authUploadDeviceSigningKeys finish", 
-                    true, result, extra)
-            }).catch((error) => {
-                console.log("authUploadDeviceSigningKeys finish", false, error)
-            })
-            await authLogic.submitAuthDict(
-                {
-                    "type": "m.login.password",
-                    "user": "@testuser2:spurdo.xyz",
-                    "identifier": {
-                        "type": "m.id.user",
-                        "user": "@testuser2:spurdo.xyz"
+                const interactiveAuth = new InteractiveAuth({
+                    doRequest: requestCallback,
+                    busyChanged: onInteractiveAuthBusyChange,
+                    stateUpdated: (nextStage, status) => {
+                        onInteractiveAuthStateUpdate({
+                            nextStage, status
+                        })
                     },
-                    "password": "123123"
-                }
-            )
-        },
-        setupNewCrossSigning: true,
+                    matrixClient: cl,
+                    // нам это не нужно 
+                    requestEmailToken: undefined as any
+                })
+                setInteractiveAuth(interactiveAuth)
+                onNeedUserPassword()
+                await interactiveAuth.attemptAuth()
+            },
+            setupNewCrossSigning: true,
+        }).then(() => {
+            res({
+                result: true
+            })
+        })
     })
-    // return request
+    return promise
+})
+
+submitAuthDictFx.use(async ({password, interactiveAuth}) => {
+    const cl = client()
+    await interactiveAuth.submitAuthDict(
+        {
+            "type": LOGIN_BY_PASSWORD,
+            "user": cl.getUserId(),
+            "identifier": {
+                "type": "m.id.user",
+                "user": cl.getUserId()
+            },
+            "password": password
+        }
+    )
 })
--- src/reset-cross-signing/public.ts
+++ src/reset-cross-signing/public.ts
@@ -1,5 +1,18 @@
 import { d } from "./domain"
+import { 
+    ConfirmResetCrossSigningFxResult, 
+    OnInteractiveAuthStateUpdateResult 
+} from "./types"
 
-export const confirmResetCrossSigningFx = d.effect<void, void, Error>()
+export const confirmResetCrossSigningFx = d
+    .effect<void, ConfirmResetCrossSigningFxResult, Error>()
 
-export const onNeedAuth = d.event<void>()
+
+export const submitAuthDict = d.event<string>()
+
+export const onNeedUserPassword = d.event<void>()
+export const onUserPasswordError = d.event<string>()
+
+export const onInteractiveAuthBusyChange = d.event<boolean>()
+export const onInteractiveAuthStateUpdate = d
+    .event<OnInteractiveAuthStateUpdateResult>()
